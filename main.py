99b568a8e100227438a76613be9f7553895e6132
888Lucy888 <chuggaaconroy888@gmail.com>
2021-11-17 09:38:53 -0600

    window

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import _thinker
+from tkinter import *
+window = Tk()
+
+lbl=Label(window, text="Welcome to our Bean Game", fg="purple", font=("Helvetica", 16))
+lbl.place(x=200, y=100)
+btn=Button(window, text="Start", fg="black")
+btn.place(x=270, y=300)
+
+window.title("Bean Game")
+window.geometry("600x400+10+20")
+window.mainloop()
