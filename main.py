2a916bc0d7b998799630cad43ff2963e6867fd16
Vélez <73377541+Crl-Vlz@users.noreply.github.com>
2021-11-17 19:24:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter.constants import HORIZONTAL
 from tkinter.ttk import Combobox
 
 
 LARGE_FONT= ("Verdana", 12)
-beans = int()
-currentPlayer = 1
+
 
 class menu(tk.Tk):
 
@@ -59,25 +59,23 @@ class StartPage(tk.Frame):
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
+
         tk.Frame.__init__(self, parent)
+        
         label = tk.Label(self, text="Settings", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         label0 = tk.Label(self, text="Number of Starting Beans")
         label0.pack(pady=10,padx=10)
-        data0=(21,22,23,24,25)
-        cb = Combobox(self, values=data0)
-        cb.pack(pady=10,padx=10)
+        sliderBeans = tk.Scale(self, from_=21, to=25, orient=HORIZONTAL, tickinterval=1)
+        sliderBeans.pack(padx=10, pady=10)
         label1 = tk.Label(self, text="Player 1 CPU Difficulty")
         label1.pack(pady=10,padx=10)
-        data=(1,2,3,4,5,6,7,8,9,10)
-        cb1 = Combobox(self, values=data)
         slider = tk.Scale(self, from_=1, to=10, orient=HORIZONTAL, tickinterval=1)
-        slider.pack(padx=10, pady=5)
-        cb1.pack(pady=10,padx=10)
+        slider.pack(padx=10, pady=10)
         label2 = tk.Label(self, text="Player 2 CPU Difficulty")
         label2.pack(pady=10,padx=10)
-        cb2 = Combobox(self, values=data)
-        cb2.pack(pady=10,padx=10)
+        sliderPlayer2 = tk.Scale(self, from_=1, to=10, orient=HORIZONTAL, tickinterval=1)
+        sliderPlayer2.pack(padx=10, pady=10)
         button1 = tk.Button(self, text="Continue",
                             command=lambda: controller.show_frame(PageTwo))
         button1.pack(pady=10,padx=10)
@@ -87,19 +85,33 @@ class PageTwo(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        beans = 23
-        label = tk.Label(self, text="Beans Remaining:", font=LARGE_FONT)
-        label.pack(pady=15,padx=10)
-        labelB = tk.Label(self, text=beans, font=LARGE_FONT)
-        labelB.pack(pady=10,padx=10)
-        label1 = tk.Label(self, text="Player" +str(currentPlayer)+"'s Turn", font=LARGE_FONT)
+        label = tk.Label(self, text="# Beans Remaining", font=LARGE_FONT)
+        label.pack(pady=20,padx=10)
+        label1 = tk.Label(self, text="Player1's Turn", font=LARGE_FONT)
         label1.pack(pady=10,padx=10)
         label2 = tk.Label(self, text="How many beans will you take?")
         label2.pack(pady=10,padx=10)
-        data=(1,2,3)
-        cb = Combobox(self, values=data)
-        cb.pack(pady=10,padx=10)
-        button2 = tk.Button(self, text="Continue",  #IF NO BEANS SHOW EndPage
+
+        buttonFrame = ttk.Frame(self)
+
+        button1Bean = ttk.Button(buttonFrame, text="1")
+        button2Bean = ttk.Button(buttonFrame, text="2")
+        button3Bean = ttk.Button(buttonFrame, text="3")
+
+        button1Bean.state(['disabled'])
+        button2Bean.state(['disabled'])
+        button3Bean.state(['disabled'])
+
+        button1Bean.grid(row=0, column=0)
+        button2Bean.grid(row=0, column=1)
+        button3Bean.grid(row=0, column=2)
+
+        buttonFrame.pack(pady=10, padx=10)
+
+        #data=(1,2,3)
+        #cb = Combobox(self, values=data)
+        #cb.pack(pady=10,padx=10)
+        button2 = ttk.Button(self, text="Continue",  #IF NO BEANS SHOW EndPage
                             command=lambda: controller.show_frame(PageTwo))
         button2.pack(pady=20,padx=10)
         
@@ -110,7 +122,7 @@ class EndPage(tk.Frame):
         tk.Frame.__init__(self,parent)
         label = tk.Label(self, text="Congratulations", font=LARGE_FONT)
         label.pack(pady=30,padx=10)
-        label1 = tk.Label(self, text=("Player "+str(currentPlayer)), font=LARGE_FONT)
+        label1 = tk.Label(self, text="Player ", font=LARGE_FONT)
         label1.pack(pady=20,padx=10)
         label2 = tk.Label(self, text="YOU WIN", font=LARGE_FONT)
         label2.pack(pady=20,padx=10)
