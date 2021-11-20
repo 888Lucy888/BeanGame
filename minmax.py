e570384267b52d946649617607ec7f1616a05bf0
Crl-Vlz <crlvlz0215@gmail.com>
2021-11-20 13:14:17 -0600

    Added possible blue red format

--- minmax.py
+++ minmax.py
@@ -10,7 +10,7 @@ class mmNode:
     value: int
     parent: mmNode = None
     children: list = None
-    blue: bool = False
+    blue: bool = False #Blue = Player 1 victory -- Red = Player 2 victory
 
 class mmTree:
     
@@ -23,6 +23,13 @@ class mmTree:
             parentNode = self.root
         if len(parentNode.children) < 3:
             parentNode.children.append(node)
+
+    def depth(self, node):
+        count = 0
+        while node != self.root:
+            count += 1
+            node = node.father
+        return count - 1
 
     def calculateMinMax(self, node: mmNode = None):
         if not node:
@@ -43,9 +50,22 @@ class mmTree:
             self.addNode(nodeC, node)
         if NodeR:
             self.addNode(nodeR, node)
-        if NodeL > 0:
+        if NodeL.value > 0:
             self.calculateMinMax(nodeL)
         if NodeC and NodeC.value > 0:
             self.calculateMinMax(nodeC)
         if NodeR and NodeR.value > 0:
             self.calculateMinMax(nodeR)
+
+        if node.value == 0:
+            depth = self.depth(node)
+            # Player 1 will have even depth
+            # For example... first turn beans equal to node on height 0
+            # This means player 2 will have the odd depths
+            if depth % 2 != 0:
+                # This means player 2 has no beans to grab, which means
+                # Player 1 grabbed the last one, victory for Player 2
+                # Long live PLayer 2
+                node.blue = False
+            else:
+                node.blue = True
