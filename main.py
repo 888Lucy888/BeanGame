dcf297d94ff3cc958df5762bc34948416f114352
888Lucy888 <chuggaaconroy888@gmail.com>
2021-11-23 22:05:40 -0600

    setUp

--- main.py
+++ main.py
@@ -2,11 +2,147 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter.constants import HORIZONTAL, VERTICAL
 from tkinter.ttk import Combobox
-from minmax import *
+from dataclasses import dataclass
+from random import randint
 
+@dataclass
+class mmNode:
+    pass
+
+@dataclass
+class mmNode:
+    value: int
+    parent: mmNode = None
+    children: list = None
+    blue: bool = False #Blue = Player 1 victory -- Red = Player 2 victory
+    blues: int = 0
+    reds: int = 0
+
+class mmTree:
+    
+    def __init__(self, root: mmNode):
+        self.root = root
+
+        self.calculateMinMax()
+        self.redPriority()
+        self.bluePriority()
+
+        print("UWU")
+        print(self.root.blues)
+        print(self.root.reds)
+
+
+    def addNode(self, node: mmNode, parentNode: mmNode = None) -> None:
+        if not parentNode:
+            parentNode = self.root
+        if not parentNode.children:
+            parentNode.children = []
+        parentNode.children.append(node)
+
+    def depth(self, node):
+        count = 0
+        while node != self.root:
+            count += 1
+            node = node.parent
+        return count - 1
+
+    def calculateMinMax(self, node: mmNode = None):
+        if not node:
+            node = self.root
+
+        print("AAAA")
+        print(node.value)
+
+        NodeR = None
+        NodeC = None
+        NodeL = None
+
+        value = int(node.value)
+
+        if value > 2:
+            NodeR = mmNode(value-3, node)
+        if int(value) > 1:
+            NodeC = mmNode(value-2, node)
+        NodeL = mmNode(value-1, node)
+        if int(NodeL.value) > 0:
+            self.addNode(NodeL, node)
+        if NodeC:
+            self.addNode(NodeC, node)
+        if NodeR:
+            self.addNode(NodeR, node)
+        if int(NodeL.value) > 0:
+            self.calculateMinMax(NodeL)
+        if NodeC and int(NodeC.value) > 0:
+            self.calculateMinMax(NodeC)
+        if NodeR and int(NodeR.value) > 0:
+            self.calculateMinMax(NodeR)
+
+        if value == 1:
+            depth = self.depth(node)
+            # Player 1 will have even depth
+            # For example... first turn beans equal to node on height 0
+            # This means player 2 will have the odd depths
+            if depth % 2 == 0:
+                # This means player 1 has to grab the last one, which means
+                # victory for Player 2
+                # Long live PLayer 2
+                self.convertRed(node)
+            else:
+                self.convertBlue(node)
+
+    # TODO we can add a priority function that counts blues and reds of each subtree
+    # To give each branch a given priority
+
+    def convertBlue(self, node: mmNode):
+        while True:
+            node.blue = True
+            node = node.parent
+            if node == self.root: break
+
+    def convertRed(self, node: mmNode):
+        while True:
+            node.blue = False
+            node = node.parent
+            if node == self.root: break
+
+    def redPriority(self, node:mmNode = None):
+        if not node:
+            node = self.root
+
+        priority = 0
+
+        if not node.blue: priority += 1
+
+        if node.children: 
+            for nodes in node.children:
+                priority += self.redPriority(nodes)
+
+        node.reds = priority
+
+        return priority
+
+    def bluePriority(self, node:mmNode = None):
+        if not node:
+            node = self.root
+
+        priority = 0
+
+        if node.blue: priority += 1
+
+        if node.children:
+            for nodes in node.children:
+                priority += self.bluePriority(nodes)
+
+        node.blues = priority
+
+        return priority
+
+dificultad= 0
+turno= False
 totalBeans = 5
 cpu1 = int()
 cpu2 = int()
+actualNodo = None
 
 class menu(tk.Tk):
 
@@ -33,7 +169,6 @@ class menu(tk.Tk):
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
-
         frame = self.frames[cont]
         frame.tkraise()
 
@@ -46,6 +181,14 @@ class menu(tk.Tk):
         cpu1 = p1Dif
         global cpu2
         cpu2 = p2Dif
+        arbol= mmTree(mmNode(totalBeans))
+        global actualNodo
+        actualNodo= arbol.root
+
+    def pick_beans(self, button, number):
+        global totalBeans
+        if totalBeans-1<number:
+            button.state(["disabled"])
 
 
         
@@ -122,22 +265,9 @@ class PageTwo(tk.Frame):
 
         buttonFrame = ttk.Frame(self)
 
-        button1Bean = ttk.Button(buttonFrame, text="1")
-        button2Bean = ttk.Button(buttonFrame, text="2")
-        button3Bean = ttk.Button(buttonFrame, text="3")
-
-        global totalBeans
-        print(totalBeans)
-        if totalBeans<3:     
-            button3Bean.state(['disabled'])
-        if totalBeans<2:
-            button2Bean.state(['disabled'])
-        if totalBeans<1:
-            button1Bean.state(['disabled'])
-        if totalBeans>3:
-            button1Bean.state(['!disabled'])
-            button1Bean.state(['!disabled'])
-            button1Bean.state(['!disabled'])
+        button1Bean = ttk.Button(buttonFrame, text="1", command=lambda: controller.pick_beans(self, 1))
+        button2Bean = ttk.Button(buttonFrame, text="2", command=lambda: controller.pick_beans(self, 2))
+        button3Bean = ttk.Button(buttonFrame, text="3", command=lambda: controller.pick_beans(self, 3))
 
 
         button1Bean.grid(row=0, column=0)
